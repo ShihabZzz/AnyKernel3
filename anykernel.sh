ae7a9e79d21928372faf852bf331fa84ef084e5f
ShihabZzz <Shihab.riadn@gmail.com>
2020-08-24 02:43:44 +0600

    Init Quark
    Signed-off-by: ShihabZzz <Shihab.riadn@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,22 +4,21 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=
+kernel.string=Quark Kernel for Redmi 4 (markw)
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=
-supported.versions=
+do.cleanuponabort=1
+device.name1=markw
+supported.versions=10
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=;
+block=/dev/block/platform/soc/7824900.sdhci/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
@@ -53,4 +52,3 @@ write_boot;
 
 #flash_boot;
 ## end vendor_boot install
-
